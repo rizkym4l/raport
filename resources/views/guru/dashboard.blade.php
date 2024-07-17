d4a32d989c15d6dbedf599950270e3a1114728f5
LouChall <louismarchal237@gmail.com>
2024-07-17 16:06:24 +0700

    Detail Upload Nilai

--- resources/views/guru/dashboard.blade.php
+++ resources/views/guru/dashboard.blade.php
@@ -4,7 +4,8 @@
 
 @section('contents')
     <div class="container p-5 mx-auto py-8">
-        {{ $tingkat }}
+        <h2 class="text-2xl font-semibold">Kelas: {{ $class->nama_kelas }} || Tingkat: {{ $tingkat }}</h2> 
+        <h2 class="text-2xl font-semibold">Mapel: {{ $Mapel->nama }} || Semester: {{$semester}}</h2>
         @if (session('error'))
             @if (session('success'))
                 <div class="bg-green-100 hidden text-green-700 p-4 rounded mb-6">
@@ -25,7 +26,7 @@
         @endif
 
 
-        <h2 class="text-2xl font-semibold">Input Nilai Siswa</h2>
+        <h2 class="mt-5 text-2xl font-semibold">Input Nilai Siswa</h2>
         <p class="mb-6">Silahkan Unduh Template <a href="{{ route('nilai.export') }}"><u>Di Sini</u></a></p>
         <form
             action="{{ route('nilai.import', ['tingkat' => $tingkat, 'kelas' => $kelas, 'mapel' => $mapel, 'semester' => $semester, 'nilai' => $nilai]) }}"
@@ -41,6 +42,26 @@
             <button
                 class="text-white bg-blue-700 my-2 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-1.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 type="submit">Import</button>
+        </form>
+
+        <form action="{{ route('nilai.import', ['tingkat' => $tingkat, 'kelas' => $kelas, 'mapel' => $mapel, 'semester' => $semester, 'nilai' => $nilai]) }}"
+            method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="flex items-center justify-center w-full">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50  hover:bg-gray-100 dark:border-gray-600">
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg class="w-8 h-8 mb-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p class="mb-2 text-sm text-gray-500 "><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p class="text-xs text-gray-500 ">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                    </div>
+                    <input id="dropzone-file" type="file" name="file" class="hidden" />
+                </label>
+            </div> 
+            <button
+            class="text-white bg-blue-700 my-2 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-1.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="submit">Import</button>
         </form>
 
 
